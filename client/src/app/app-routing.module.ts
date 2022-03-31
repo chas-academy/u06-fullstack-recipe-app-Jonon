4defa3bc94c839ad370982b46eec5c1c102fa5a8
Jonathan Jonsson <jonathancjonsson@outlook.com>
2022-03-31 19:17:41 +0200

    Add notFound component to routes

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NotFoundComponent } from './not-found/not-found.component';
 import { RecipesListComponent } from './recipes/recipes-list/recipes-list.component';
 
 
 const routes: Routes = [
   {path: "", redirectTo: "recipes", pathMatch: "full" },
   {path: "recipes", component: RecipesListComponent},
+  {path: "**", component: NotFoundComponent}
 ];
 
 @NgModule({
