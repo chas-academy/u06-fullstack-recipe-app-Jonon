655027eb0aa209571602a578bcd7aa0f55b2551b
Jonathan Jonsson <jonathancjonsson@outlook.com>
2022-04-04 20:55:52 +0200

    Use http in constructor

--- client/src/app/services/recipe.service.ts
+++ client/src/app/services/recipe.service.ts
@@ -1,9 +1,10 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RecipeService {
-
-  constructor() { }
+  constructor(private http: HttpClient) {}
 }
