214dee0754645b985dd913533a0d9c1fc975e310
Jonathan Jonsson <jonathancjonsson@outlook.com>
2022-04-04 21:02:52 +0200

    Use RecipeSErvice

--- client/src/app/recipes/recipes-list/recipes-list.component.ts
+++ client/src/app/recipes/recipes-list/recipes-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { RecipeService } from 'src/app/services/recipe.service';
+import { Recipe, edamamAPI } from '../recipe';
 
 @Component({
   selector: 'app-recipes-list',
@@ -6,10 +8,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipes-list.component.css']
 })
 export class RecipesListComponent implements OnInit {
+  constructor(private recipeService: RecipeService) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
