6942e5ce29b3da9f5616387274f5185fa80af477
Jonathan Jonsson <jonathancjonsson@outlook.com>
2022-04-04 22:17:25 +0200

    Add Api url

--- client/src/app/services/recipe.service.ts
+++ client/src/app/services/recipe.service.ts
@@ -2,10 +2,14 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
+import { environment } from 'src/environments/environment';
+
 @Injectable({
   providedIn: 'root'
 })
 export class RecipeService {
+  API_URL = `https://api.edamam.com/api/recipes/v2?type=public&q=mexico&app_id=${environment.app_id}&app_key=${environment.api_key}`;
+
   constructor(private http: HttpClient) {}
 
   getAll(): Observable<any> {
