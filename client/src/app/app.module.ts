36136bba7b88acc194fc05a9c3f18db2d8e55eba
Jonathan Jonsson <jonathancjonsson@outlook.com>
2022-04-04 21:01:06 +0200

    Use HttpsClientModule

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -7,17 +8,18 @@ import { NotFoundComponent } from './not-found/not-found.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 
-import { CommonModule } from '@angular/common';
 import { RecipesModule } from './recipes/recipes.module';
+import { RecipeService } from './services/recipe.service';
+
 @NgModule({
   declarations: [
     AppComponent,
     NotFoundComponent,
     HeaderComponent,
-    FooterComponent,
+    FooterComponent
   ],
-  imports: [BrowserModule, AppRoutingModule, RecipesModule],
-  providers: [],
-  bootstrap: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, RecipesModule, HttpClientModule],
+  providers: [RecipeService],
+  bootstrap: [AppComponent]
 })
 export class AppModule {}
