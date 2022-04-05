8912767b1cab7dca86bcd9356c2f806dd562ba7a
Jonathan Jonsson <jonathancjonsson@outlook.com>
2022-04-05 20:18:04 +0200

    Create new component recipe-details

--- client/src/app/recipes/recipes.module.ts
+++ client/src/app/recipes/recipes.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { RecipesRoutingModule } from './recipes-routing.module';
 import { RecipesListComponent } from './recipes-list/recipes-list.component';
+import { RecipeDetailsComponent } from './recipe-details/recipe-details.component';
 
 @NgModule({
-  declarations: [RecipesListComponent],
+  declarations: [RecipesListComponent, RecipeDetailsComponent],
   imports: [CommonModule, RecipesRoutingModule],
 })
 export class RecipesModule {}
