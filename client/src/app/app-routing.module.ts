50aeaca1a3623f857229437bcd79fd2100738f73
Jonathan Jonsson <jonathancjonsson@outlook.com>
2022-04-06 12:00:16 +0200

    Add recipes route with id parameter

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -2,16 +2,17 @@ import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { RecipesListComponent } from './recipes/recipes-list/recipes-list.component';
-
+import { RecipeDetailsComponent } from './recipes/recipe-details/recipe-details.component';
 
 const routes: Routes = [
-  {path: "", redirectTo: "recipes", pathMatch: "full" },
-  {path: "recipes", component: RecipesListComponent},
-  {path: "**", component: NotFoundComponent}
+  { path: '', redirectTo: 'recipes', pathMatch: 'full' },
+  { path: 'recipes', component: RecipesListComponent },
+  { path: 'recipes/:id', component: RecipeDetailsComponent },
+  { path: '**', component: NotFoundComponent }
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
