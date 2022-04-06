6fe37f3914f2d8fc7ddd4770d64638dd914159f2
Jonathan Jonsson <jonathancjonsson@outlook.com>
2022-04-06 16:49:33 +0200

    Get recipe Id

--- client/src/app/recipes/recipes-list/recipes-list.component.ts
+++ client/src/app/recipes/recipes-list/recipes-list.component.ts
@@ -1,3 +1,4 @@
+import { registerLocaleData } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { RecipeService } from 'src/app/services/recipe.service';
 import { Recipe, edamamAPI } from '../recipe';
@@ -9,12 +10,14 @@ import { Recipe, edamamAPI } from '../recipe';
 })
 export class RecipesListComponent implements OnInit {
   recipes: any = [];
+  recipeId: any = [];
 
   constructor(private recipeService: RecipeService) {}
 
   ngOnInit(): void {
     this.recipeService.getAll().subscribe((data: edamamAPI) => {
       this.recipes = data.hits.map((res) => res.recipe);
+      this.recipeId = data.hits.map((res) => res.recipe.uri.split('_')[1]);
     });
   }
 }
