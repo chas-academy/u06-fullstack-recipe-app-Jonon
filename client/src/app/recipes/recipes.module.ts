f7ea94e796e5560834bf6bf86b1cd8425010170a
Jonathan Jonsson <jonathancjonsson@outlook.com>
2022-04-07 23:02:41 +0200

    Create search component

--- client/src/app/recipes/recipes.module.ts
+++ client/src/app/recipes/recipes.module.ts
@@ -4,9 +4,10 @@ import { CommonModule } from '@angular/common';
 import { RecipesRoutingModule } from './recipes-routing.module';
 import { RecipesListComponent } from './recipes-list/recipes-list.component';
 import { RecipeDetailsComponent } from './recipe-details/recipe-details.component';
+import { SearchComponent } from './search/search.component';
 
 @NgModule({
-  declarations: [RecipesListComponent, RecipeDetailsComponent],
+  declarations: [RecipesListComponent, RecipeDetailsComponent, SearchComponent],
   imports: [CommonModule, RecipesRoutingModule],
 })
 export class RecipesModule {}
