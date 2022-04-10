8eca0fa6cb97961353674f6256d4601f1bc8d2c5
Jonathan Jonsson <jonathancjonsson@outlook.com>
2022-04-10 16:33:16 +0200

    use search query

--- client/src/app/services/recipe.service.ts
+++ client/src/app/services/recipe.service.ts
@@ -10,17 +10,18 @@ import { environment } from 'src/environments/environment';
 export class RecipeService {
   API_URL_BASE = 'https://api.edamam.com/api/recipes/v2/';
   API_URL_TYPE = '?type=public&';
-  API_URL_Q = 'q=mexico&';
   API_ID = `app_id=${environment.app_id}&`;
   API_KEY = `app_key=${environment.api_key}`;
 
   constructor(private http: HttpClient) {}
 
-  getAll(): Observable<any> {
+  getAll(q: string): Observable<any> {
     return this.http.get<any>(
       this.API_URL_BASE +
         this.API_URL_TYPE +
-        this.API_URL_Q +
+        'q=' +
+        q +
+        '&' +
         this.API_ID +
         this.API_KEY
     );
