875d586e84e1c70b3da5df5d9ce920fbbc697048
Jonathan Jonsson <jonathancjonsson@outlook.com>
2022-04-10 16:31:18 +0200

    Import ReactiveFormsModule

--- client/src/app/recipes/recipes.module.ts
+++ client/src/app/recipes/recipes.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { RecipesRoutingModule } from './recipes-routing.module';
 import { RecipesListComponent } from './recipes-list/recipes-list.component';
@@ -8,6 +10,6 @@ import { SearchComponent } from './search/search.component';
 
 @NgModule({
   declarations: [RecipesListComponent, RecipeDetailsComponent, SearchComponent],
-  imports: [CommonModule, RecipesRoutingModule],
+  imports: [CommonModule, ReactiveFormsModule, RecipesRoutingModule]
 })
 export class RecipesModule {}
