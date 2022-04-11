4c117a0c8447216f9c3f59852961b670a2abafec
Jonathan Jonsson <jonathancjonsson@outlook.com>
2022-04-11 23:15:28 +0200

    Add form select options

--- client/src/app/recipes/search/search.component.ts
+++ client/src/app/recipes/search/search.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-search',
@@ -7,11 +8,35 @@ import { FormBuilder } from '@angular/forms';
   styleUrls: ['./search.component.css']
 })
 export class SearchComponent implements OnInit {
-  searchForm = this.fb.group({
-    search: ['']
-  });
+  health = [
+    { name: '' },
+    { name: 'alcohol-cocktail' },
+    { name: 'dairy-free' },
+    { name: 'DASH' },
+    { name: 'egg-free' },
+    { name: 'fish-free' },
+    { name: 'fodmap-free' },
+    { name: 'gluten-free' },
+    { name: 'immuno-supportive' },
+    { name: 'keto-friendly' },
+    { name: 'kidney-friendly' },
+    { name: 'kosher' },
+    { name: 'low-fat-abs' },
+    { name: 'low-potassium' },
+    { name: 'low-sugar' },
+    { name: 'vegan' }
+  ];
 
-  constructor(private fb: FormBuilder) {}
+  searchForm: FormGroup;
+
+  constructor(private router: Router, private fb: FormBuilder) {
+    this.searchForm = this.fb.group({
+      search: '',
+      health: ''
+    });
+
+    this.searchForm.valueChanges.subscribe(console.log);
+  }
 
   ngOnInit(): void {}
 }
