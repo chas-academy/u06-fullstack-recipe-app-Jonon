35ed54b8fd9d0d377ae4f8801bb567714db8e7a5
Jonathan Jonsson <jonathancjonsson@outlook.com>
2022-04-12 15:15:14 +0200

    Import fontawesomeModule

--- client/src/app/recipes/recipes.module.ts
+++ client/src/app/recipes/recipes.module.ts
@@ -8,8 +8,15 @@ import { RecipesListComponent } from './recipes-list/recipes-list.component';
 import { RecipeDetailsComponent } from './recipe-details/recipe-details.component';
 import { SearchComponent } from './search/search.component';
 
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+
 @NgModule({
   declarations: [RecipesListComponent, RecipeDetailsComponent, SearchComponent],
-  imports: [CommonModule, ReactiveFormsModule, RecipesRoutingModule]
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    RecipesRoutingModule,
+    FontAwesomeModule
+  ]
 })
 export class RecipesModule {}
