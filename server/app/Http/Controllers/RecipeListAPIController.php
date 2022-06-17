c50ecb9011ba344185a2dfefcaa008aaae9f825a
Jonathan Jonsson <jonathancjonsson@outlook.com>
2022-06-17 23:14:23 +0200

    Add logic to controller to create recipe list

--- server/app/Http/Controllers/RecipeListAPIController.php
+++ server/app/Http/Controllers/RecipeListAPIController.php
@@ -11,4 +11,16 @@ class RecipeListAPIController extends Controller
         $recipeLists = RecipeList::get()->toJson(JSON_PRETTY_PRINT);
         return response($recipeLists, 200);
     }
+
+    public function createRecipeList(Request $request) {
+        $recipeList = new RecipeList();
+        $recipeList->name = $request->name;
+        $recipeList->save();
+
+        return response()->json([
+            "message" => "Recipe List created"
+        ], 201);
+    }
+
+    
 }
