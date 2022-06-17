2ffbaef0e5dcc623a8783cbd145c6249a1c1d374
Jonathan Jonsson <jonathancjonsson@outlook.com>
2022-06-17 22:51:07 +0200

    Add logic to get all recipe lists

--- server/app/Http/Controllers/RecipeListAPIController.php
+++ server/app/Http/Controllers/RecipeListAPIController.php
@@ -7,5 +7,8 @@ use Illuminate\Http\Request;
 
 class RecipeListAPIController extends Controller
 {
-    
+    public function getAllRecipeLists() {
+        $recipeLists = RecipeList::get()->toJson(JSON_PRETTY_PRINT);
+        return response($recipeLists, 200);
+    }
 }
