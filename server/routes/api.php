c4f9f18c78e726af2189ae5effef7edd678ee621
Jonathan Jonsson <jonathancjonsson@outlook.com>
2022-06-18 14:53:02 +0200

    Change request method to post to get login to work

--- server/routes/api.php
+++ server/routes/api.php
@@ -18,7 +18,7 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::post('register', [AuthController::class, 'register']);
-Route::get('login', [AuthController::class, 'login']);
+Route::post('login', [AuthController::class, 'login']);
 
 Route::group(['middleware' => 'auth:sanctum'], function() {
     // log out user
