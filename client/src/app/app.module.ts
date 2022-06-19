bb26d5e4122202ebd58440dc9051bb9245114c8c
Jonathan Jonsson <jonathancjonsson@outlook.com>
2022-06-19 11:31:20 +0200

    declare registerComponent and RecipeListsComponent

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { RecipeService } from './services/recipe.service';
 import { LoginComponent } from './login/login.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { RecipeListsComponent } from './recipe-lists/recipe-lists.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
@@ -22,7 +23,8 @@ import { RecipeListsComponent } from './recipe-lists/recipe-lists.component';
     HeaderComponent,
     FooterComponent,
     LoginComponent,
-    RecipeListsComponent
+    RecipeListsComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
