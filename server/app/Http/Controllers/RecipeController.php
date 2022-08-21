61ef74dac17f7917e20e6bc0ac772c9e196a46c2
Jonathan Jonsson <jonathancjonsson@outlook.com>
2022-08-21 21:23:16 +0200

    Add logic to get recipes by recipe list id

--- server/app/Http/Controllers/RecipeController.php
+++ server/app/Http/Controllers/RecipeController.php
@@ -2,9 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Recipe;
 use Illuminate\Http\Request;
 
 class RecipeController extends Controller
 {
-    
+    public function index() {
+      $recipe_list_id = request('recipe_list_id');
+
+      $recipe = Recipe::where('recipe_list_id', $recipe_list_id)->get();
+        return response()->json($recipe, 200);
+    }
 }
