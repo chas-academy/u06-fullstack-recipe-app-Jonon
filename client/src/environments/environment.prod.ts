4092f54a3db70678eeb4ad5432d33f166a1362b1
Jonathan <jonathancjonsson@gmail.com>
2023-08-13 11:36:55 +0200

    Fix cors error

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   APP_ID: 'bfaf4ffb',
   API_KEY: '433472119c46cbf02de4c7bd7bccf01b',
-  RECIPE_LIST_API: 'https://recipelistapi.herokuapp.com/api'
+  RECIPE_LIST_API: 'https://recipe-app-server-obms.onrender.com/api'
 };
